7bd74eaa1e2f89167a8ebee8be34e18879c9492f
Austin0422 <92912396+Austin0422@users.noreply.github.com>
2021-12-21 01:38:47 +0800

    Update ChessPiece.java
    2.0

--- Project/src/model/ChessPiece.java
+++ Project/src/model/ChessPiece.java
@@ -3,7 +3,7 @@ package model;
 import java.awt.*;
 
 public enum ChessPiece {
-    BLACK(Color.BLACK,-1), WHITE(Color.WHITE,1), BLUE(new Color(60, 150, 200), 2); //转化为二维数组时黑棋-1 白棋为1
+    BLACK(Color.BLACK,-1), WHITE(Color.WHITE,1); /*BLUE(new Color(60, 150, 200), 2); //转化为二维数组时黑棋-1 白棋为1*/
 
     private final Color color;
     private final int represent;
